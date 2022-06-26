a006e717ca6ba9f8addecbbc6920300f6c334783
Gang Wen <gang.wen@ingka.ikea.com>
2022-06-26 20:17:00 +0800

    Added tailwindcss 3.1

--- tailwind31/src/App.tsx
+++ tailwind31/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p className='font-semibold'>
+          Edit <code className="py-2">src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
